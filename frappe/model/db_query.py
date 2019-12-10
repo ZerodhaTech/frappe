c20b1654bd02fae151fa6eeea7ecd4985a0412df
Shridhar <shridhar.p@zerodha.com>
2019-12-10 16:51:41 +0530

    Bug fixed [Automatically set to ID=undefined]

--- frappe/model/db_query.py
+++ frappe/model/db_query.py
@@ -495,6 +495,10 @@ class DatabaseQuery(object):
 				value = f.value or "''"
 				fallback = "''"
 
+			elif f.fieldname == 'name':
+				value = f.value or "''"
+				fallback = "''"
+
 			else:
 				value = flt(f.value)
 				fallback = 0
